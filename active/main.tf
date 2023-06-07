557058a648b7b6cf76bb73cc0cea33688ec37e2b
Brian Leung <brian.leungkp@gmail.com>
2023-06-07 14:54:20 +0800

    active passive cluster

--- active/main.tf
+++ active/main.tf
@@ -20,13 +20,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.19.0"
 
-  name = "education-vpc"
+  name = "${var.project}-vpc"
 
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  cidr = var.network-ip-range
+  azs  = slice(data.aws_availability_zones.available.names, 0, 2)
 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets = var.private-net
+  public_subnets  = var.public-net
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -43,89 +43,90 @@ module "vpc" {
   }
 }
 
-resource "aws_iam_role" "eks-iam-role" {
-  name = "devopsthehardway-eks-iam-role"
+# resource "aws_iam_role" "eks-iam-role" {
+#   name = "${var.project}-eks-iam-role"
 
-  path = "/"
+#   path = "/"
 
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-   ]
-  }
-  EOF
-}
+#   assume_role_policy = <<EOF
+#   {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "eks.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#    ]
+#   }
+#   EOF
+# }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role    = aws_iam_role.eks-iam-role.name
-}
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role    = aws_iam_role.eks-iam-role.name
-}
+# resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#   role    = aws_iam_role.eks-iam-role.name
+# }
+# resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   role    = aws_iam_role.eks-iam-role.name
+# }
 
 
-resource "aws_eks_cluster" "devopsthehardway-eks" {
-  name = "devopsthehardway-cluster"
-  role_arn = aws_iam_role.eks-iam-role.arn
+resource "aws_eks_cluster" "eks" {
+  name = "${var.project}-cluster"
+  role_arn = var.iam-role-eks-arn
 
   vpc_config {
     subnet_ids = module.vpc.public_subnets
   }
 
+  # TODO
   depends_on = [
-    aws_iam_role.eks-iam-role,
+    # aws_iam_role.eks-iam-role,
   ]
 }
 
-resource "aws_iam_role" "workernodes" {
-  name = "eks-node-group-example"
+# resource "aws_iam_role" "workernodes" {
+#   name = "eks-node-group-example"
  
-  assume_role_policy = jsonencode({
-   Statement = [{
-    Action = "sts:AssumeRole"
-    Effect = "Allow"
-    Principal = {
-     Service = "ec2.amazonaws.com"
-    }
-   }]
-   Version = "2012-10-17"
-  })
-}
+#   assume_role_policy = jsonencode({
+#    Statement = [{
+#     Action = "sts:AssumeRole"
+#     Effect = "Allow"
+#     Principal = {
+#      Service = "ec2.amazonaws.com"
+#     }
+#    }]
+#    Version = "2012-10-17"
+#   })
+# }
  
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role    = aws_iam_role.workernodes.name
-}
+# resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+#   role    = aws_iam_role.workernodes.name
+# }
  
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role    = aws_iam_role.workernodes.name
-}
+# resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+#   role    = aws_iam_role.workernodes.name
+# }
  
-resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
-  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
-  role    = aws_iam_role.workernodes.name
-}
+# resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+#   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+#   role    = aws_iam_role.workernodes.name
+# }
  
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role    = aws_iam_role.workernodes.name
-}
+# resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   role    = aws_iam_role.workernodes.name
+# }
 
 resource "aws_eks_node_group" "worker-node-group" {
-  cluster_name  = aws_eks_cluster.devopsthehardway-eks.name
-  node_group_name = "devopsthehardway-workernodes"
-  node_role_arn  = aws_iam_role.workernodes.arn
+  cluster_name  = aws_eks_cluster.eks.name
+  node_group_name = "${var.project}-workernodes"
+  node_role_arn  = var.iam-role-node-arn
   subnet_ids   = module.vpc.public_subnets
   instance_types = ["t2.micro"]
  
@@ -135,10 +136,11 @@ resource "aws_eks_node_group" "worker-node-group" {
     min_size   = 1
   }
  
+  # TODO
   depends_on = [
-   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+   # aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   # aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   ##aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
 }
 
